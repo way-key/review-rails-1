71eba8ba941042ac6f61fa021db2428fc9e6bb64
way-key <45gqz7@gmail.com>
2020-04-07 18:20:42 +0900

    [Start,finish]4/7

---
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '16c9c12f242e10fb751f3b233dc5873d8f8c685660be1f4df4f32eef7c252bc1a8f9c6ed4a14fa92e1c53ae8d3d2b66caec5fa63a5da58eb95ff18cf6de62797'
